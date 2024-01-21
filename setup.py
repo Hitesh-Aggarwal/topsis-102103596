fceefe4b061114ce37aebecf28461b4f6c1e67cb
Hitesh Aggarwal <hitesh33231@gmail.com>
2024-01-22 01:41:00 +0530

    fix classifiers

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setup(
     ],  # Keywords that define your package best
     install_requires=["pandas", "numpy", "scipy"],  # I get to this in a second
     classifiers=[
-        "Development Status :: 3 Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
-        "Topic :: Data Analytics :: Multi Criteria Decision Making",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",  # Again, pick a license
         "Programming Language :: Python :: 3.11",  # Specify which pyhton versions that you want to support
         "Programming Language :: Python :: 3.10",  # Specify which pyhton versions that you want to support
